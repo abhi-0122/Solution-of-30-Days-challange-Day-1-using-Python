7d000cbc532c3071f6fdeb1ffa4ce689a6647933
Kumar Abhishek <59106601+abhi-0122@users.noreply.github.com>
2020-03-29 16:41:42 +0530

    Add files via upload

---
+++ prac.py
@@ -0,0 +1,17 @@
+#Author: Kumar Abhishek; Date: March 29, 2020
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+j = int(input())
+e = float(input())
+t = input()
+# Print the sum of both integer variables on a new line.
+print(i+j)
+# Print the sum of the double variables on a new line.
+print(d+e)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+t)
